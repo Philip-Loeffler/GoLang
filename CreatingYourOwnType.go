6e86adb36180c1ff6f24d22aff200b0b945cb9e5
ploeffler <Philip.Loeffler@ascendum.com>
2019-05-27 15:36:37 -0400

    go practice

---
+++ CreatingYourOwnType.go
@@ -0,0 +1,22 @@
+package main
+
+var a int
+
+type hotdog int
+
+// static = when you declare a variable is of a type,
+// it does not change
+// creating your own type
+var b hotdog
+
+func main34() {
+	a22 = 23
+	b22 = 24
+
+	fmt.println(a22)
+	fmt.println(b22)
+
+	// this will not work, because you cannot have something that
+	// is type int be equal to something that is type "hotdog"
+	a = b
+}
