f72df875f89f4707a2ef7ce10069458b3de15aef
ploeffler <philiploeffler@gmail.com>
2019-06-23 10:03:51 -0400

    compositeliterals

---
+++ compositiedata.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+// a composite data type is any date type which can be constructed in a program
+// using the programmnig languages primitive data types and other composite types.
+// it is sometimes called a structure or aggregate data type.
+func compositeDataTypes() {
+ // x :=  type {values} 
+ // this is a composite literal
+// slice allows you to group values of the same type. 
+x := []int {4,5,7,8,42}
+fmt.Println(x)
+
+}
